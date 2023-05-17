dbf1cf44e108708ba92b84d51dae02a31c8fb22d
Dominik Pierdiluca <wi22b124@technikum-wien.at>
2023-05-17 12:23:39 +0200

    first commit, added project

---
+++ src/main/java/com/example/maintenancemonitorgrp15/MaintenanceMonitorGrp15Application.java
@@ -0,0 +1,13 @@
+package com.example.maintenancemonitorgrp15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonitorGrp15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceMonitorGrp15Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maintenancemonitorgrp15/MaintenanceMonitorGrp15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maintenancemonitorgrp15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorGrp15ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
