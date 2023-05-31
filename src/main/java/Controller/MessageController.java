da772e69ecd87f9dd13db854058f9aed54a869b2
Lukas Huber <lukashuber2020@gmail.com>
2023-05-31 16:19:36 +0200

    Added feature/set

--- src/main/java/Controller/MessageController.java
+++ src/main/java/Controller/MessageController.java
@@ -1,14 +1,23 @@
 package Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class MessageController {
 
+
     String ogMessage = "Everything operates as expected"; // Default message
+    String currentMessage = ogMessage;
 
     @RequestMapping("/api/message")
     public String showMessage(){
         return ogMessage; //returns the current message of the string ogMessage
+    }
+
+    @RequestMapping("/api/message/set")
+    public String setMessage(@RequestParam String customMessage){
+        currentMessage = customMessage;
+        return "ok";
     }
 
 
