5156d2b3ff801e3e5a9ff99795224f84fc0188f3
RaiFAL0 <97065610+RaiFAL0@users.noreply.github.com>
2023-06-23 16:10:16 +0200

    Update MessageController.java

--- src/main/java/Controller/MessageController.java
+++ src/main/java/Controller/MessageController.java
@@ -2,7 +2,9 @@ package Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MessageController {
 
 
